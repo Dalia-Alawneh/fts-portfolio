a9b9b28b58b3136e232b948197c4af34e653718b
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-02-28 00:44:13 +0200

    Add skills carousel

--- js/index.js
+++ js/index.js
@@ -4,6 +4,14 @@ const menu = document.querySelector('.nav__list');
 const nav = document.querySelector('.nav')
 const list = document.querySelector('.sm-nav__list--positioned-center')
 
+const carousel = document.querySelector('.carousel')
+let innerContainer = document.querySelector(".inner-container");
+
+let pressed = false;
+let startX;
+let x;
+
+
 menu__btn.addEventListener('click', () => {
   menu.classList.add('show')
   menu.classList.remove('hide')
@@ -28,3 +36,40 @@ window.addEventListener("scroll", function () {
   }
 });
 
+
+
+carousel.addEventListener('mousedown', (e) => {
+  pressed = true;
+  startX = e.offsetX - innerContainer.offsetLeft;
+  carousel.style.cursor = 'grabbing'
+})
+carousel.addEventListener('mouseenter', (e) => {
+  carousel.style.cursor = 'grab'
+})
+carousel.addEventListener('mouseup', (e) => {
+  carousel.style.cursor = 'grab'
+})
+window.addEventListener('mouseup', (e) => {
+  pressed = false
+})
+carousel.addEventListener('mousemove', (e) => {
+  if (!pressed) return;
+  e.preventDefault();
+  x = e.offsetX
+  innerContainer.style.left = `${x - startX}px`
+  checkBoundary()
+})
+
+function checkBoundary() {
+  let outer = carousel.getBoundingClientRect();
+  let inner = innerContainer.getBoundingClientRect();
+
+
+  if (parseInt(innerContainer.style.left) > 0) {
+    innerContainer.style.left = "0px";
+  }
+
+  // if (inner.right < outer.right) {
+  //   innerContainer.style.left = `${inner.width - outer.width}px`;
+  // }
+}
