ccbcf3adf37e360f10d6d4f0c23bcc306622a190
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-03-01 20:05:54 +0200

    Refactor code

--- js/index.js
+++ js/index.js
@@ -39,42 +39,6 @@ window.addEventListener("scroll", function () {
 
 
 
-carousel.addEventListener('mousedown', (e) => {
-  pressed = true;
-  startX = e.offsetX - innerContainer.offsetLeft;
-  carousel.style.cursor = 'grabbing'
-})
-carousel.addEventListener('mouseenter', (e) => {
-  carousel.style.cursor = 'grab'
-})
-carousel.addEventListener('mouseup', (e) => {
-  carousel.style.cursor = 'grab'
-})
-window.addEventListener('mouseup', (e) => {
-  pressed = false
-})
-carousel.addEventListener('mousemove', (e) => {
-  if (!pressed) return;
-  e.preventDefault();
-  x = e.offsetX
-  innerContainer.style.left = `${x - startX}px`
-  checkBoundary()
-})
-
-function checkBoundary() {
-  let outer = carousel.getBoundingClientRect();
-  let inner = innerContainer.getBoundingClientRect();
-
-
-  // if (parseInt(innerContainer.style.left) > 0) {
-  //   innerContainer.style.left = "0px";
-  // }
-
-  // if (inner.right < outer.right) {
-  //   innerContainer.style.left = `${inner.width - outer.width}px`;
-  // }
-}
-
 function createStars() {
   const starCount = 100;
 
